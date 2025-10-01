9ab3cf35cbd347b18290f295682c7292c28e08ea
Jose Alejandro Valencia Henao <jose.valencia1373@uco.net.co>
2025-10-01 00:21:21 -0500

    implemetacion de docker y verificacion

--- src/main/java/co/edu/uco/reactiveexample/controller/PaisController.java
+++ src/main/java/co/edu/uco/reactiveexample/controller/PaisController.java
@@ -1,5 +1,7 @@
 package co.edu.uco.reactiveexample.controller;
 
+import co.edu.uco.reactiveexample.entity.CountryEntity;
+import co.edu.uco.reactiveexample.repository.CountryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,16 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/api/v1/countries")
 public class PaisController {
 
+    private final CountryRepository repository;
+
+    // Constructor con inyección de dependencias
+    public PaisController(final CountryRepository repository) {
+        this.repository = repository;
+    }
+
+    // GET /api/v1/countries -> devuelve todos los países desde la BD
     @GetMapping
-    public Flux<String> getAllCountries() {
-        return Flux.just("Hola", " ", "validando", "...");
+    public Flux<CountryEntity> getAllCountries() {
+        return repository.findAll();
     }
 }
