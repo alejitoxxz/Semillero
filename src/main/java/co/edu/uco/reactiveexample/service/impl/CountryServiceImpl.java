1149109d4922636393942accf027e3ee061e532d
Jose Alejandro Valencia Henao <jose.valencia1373@uco.net.co>
2025-10-07 20:48:35 -0500

    correccion

--- src/main/java/co/edu/uco/reactiveexample/service/impl/CountryServiceImpl.java
+++ src/main/java/co/edu/uco/reactiveexample/service/impl/CountryServiceImpl.java
@@ -1,5 +1,46 @@
 package co.edu.uco.reactiveexample.service.impl;
 
-public class CountryServiceImpl {
+import co.edu.uco.reactiveexample.entity.CountryEntity;
+import co.edu.uco.reactiveexample.service.CustomerService;
+import reactor.core.publisher.Mono;
 
+public class CountryServiceImpl implements CustomerService{
+
+	@Override
+	public Mono<CountryEntity> create(CountryEntity conuntry) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<CountryEntity> update(int id, CountryEntity conuntry) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<CountryEntity> delete(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<CountryEntity> findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<CountryEntity> findByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<CountryEntity> findAll() {
+		// TODO Auto-generated method stub
+		return repository.findAll();
+	}
+
+	
 }
