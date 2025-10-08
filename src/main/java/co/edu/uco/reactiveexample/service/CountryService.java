188deeafc3b8dcf718c15c719e4c29cd4052c99a
Jose Alejandro Valencia <josevalenciahenao6@gmail.com>
2025-10-07 20:44:28 -0500

    Refactor country controller to use service layer

---
+++ src/main/java/co/edu/uco/reactiveexample/service/CountryService.java
@@ -0,0 +1,16 @@
+package co.edu.uco.reactiveexample.service;
+
+import co.edu.uco.reactiveexample.entity.CountryEntity;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface CountryService {
+
+    Flux<CountryEntity> getAllCountries();
+
+    Mono<CountryEntity> createCountry(CountryEntity country);
+
+    Mono<CountryEntity> updateCountry(Integer id, CountryEntity country);
+
+    Mono<Boolean> deleteCountry(Integer id);
+}
